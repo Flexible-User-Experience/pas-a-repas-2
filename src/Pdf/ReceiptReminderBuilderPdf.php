0c4f661cfb9f9a9eba7e6610d5de456e0ef0cc6f
David Romaní <david@flux.cat>
2019-11-11 20:33:37 +0100

    add PDF class

---
+++ src/Pdf/ReceiptReminderBuilderPdf.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Pdf;
+
+use App\Entity\Receipt;
+use App\Service\SmartAssetsHelperService;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use WhiteOctober\TCPDFBundle\Controller\TCPDFController;
+
+/**
+ * Class ReceiptReminderBuilderPdf.
+ *
+ * @category Service
+ */
+class ReceiptReminderBuilderPdf extends AbstractReceiptInvoiceBuilderPdf
+{
+    /**
+     * ReceiptBuilderPdf constructor.
+     *
+     * @param TCPDFController          $tcpdf
+     * @param SmartAssetsHelperService $sahs
+     * @param Translator               $ts
+     * @param string                   $pwt    project web title
+     * @param string                   $bn     boss name
+     * @param string                   $bd     boss DNI
+     * @param string                   $ba     boss address
+     * @param string                   $bc     boss city
+     * @param string                   $ib     IBAN bussines
+     * @param string                   $locale default locale useful in CLI
+     */
+    public function __construct(TCPDFController $tcpdf, SmartAssetsHelperService $sahs, Translator $ts, $pwt, $bn, $bd, $ba, $bc, $ib, $locale)
+    {
+        parent::__construct($tcpdf, $sahs, $ts, $pwt, $bn, $bd, $ba, $bc, $ib, $locale);
+    }
+
+    /**
+     * @return \TCPDF
+     */
+    public function buildBatchReminder()
+    {
+        if ($this->sahs->isCliContext()) {
+            $this->ts->setLocale($this->locale);
+        }
+
+        /** @var BaseTcpdf $pdf */
+        $pdf = $this->tcpdf->create($this->sahs);
+
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor($this->pwt);
+        $pdf->SetTitle($this->ts->trans('backend.admin.receipt_reminder.title'));
+        $pdf->SetSubject($this->ts->trans('backend.admin.invoice.detail').' '.$this->ts->trans('backend.admin.receipt_reminder.title'));
+        // set default font subsetting mode
+        $pdf->setFontSubsetting(true);
+        // remove default header/footer
+        $pdf->setPrintHeader(false);
+        $pdf->setPrintFooter(false);
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+        // set margins
+        $pdf->SetMargins(BaseTcpdf::PDF_A5_MARGIN_LEFT, BaseTcpdf::PDF_A5_MARGIN_TOP, BaseTcpdf::PDF_A5_MARGIN_RIGHT);
+        $pdf->SetAutoPageBreak(true, BaseTcpdf::PDF_A5_MARGIN_BOTTOM);
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        return $pdf;
+    }
+
+    /**
+     * @param Receipt $receipt
+     *
+     * @return \TCPDF
+     *
+     * @throws \Exception
+     */
+    public function build(Receipt $receipt)
+    {
+        if ($this->sahs->isCliContext()) {
+            $this->ts->setLocale($this->locale);
+        }
+
+        /** @var BaseTcpdf $pdf */
+        $pdf = $this->tcpdf->create($this->sahs);
+
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor($this->pwt);
+        $pdf->SetTitle($this->ts->trans('backend.admin.receipt_reminder.title').' '.$receipt->getReceiptNumber());
+        $pdf->SetSubject($this->ts->trans('backend.admin.invoice.detail').' '.$this->ts->trans('backend.admin.receipt_reminder.title').' '.$receipt->getReceiptNumber());
+        // set default font subsetting mode
+        $pdf->setFontSubsetting(true);
+        // remove default header/footer
+        $pdf->setPrintHeader(false);
+        $pdf->setPrintFooter(false);
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+        // set margins
+        $pdf->SetMargins(BaseTcpdf::PDF_A5_MARGIN_LEFT, BaseTcpdf::PDF_A5_MARGIN_TOP, BaseTcpdf::PDF_A5_MARGIN_RIGHT);
+        $pdf->SetAutoPageBreak(true, BaseTcpdf::PDF_A5_MARGIN_BOTTOM);
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // add page
+        $pdf->AddPage('L', 'A5', true, true);
+        $this->buildReceiptRemainderPageForItem($pdf, $receipt);
+
+        return $pdf;
+    }
+
+    /**
+     * @param BaseTcpdf|\TCPDF $pdf
+     * @param Receipt          $receipt
+     *
+     * @throws \Exception
+     */
+    public function buildReceiptRemainderPageForItem($pdf, Receipt $receipt)
+    {
+        // logo
+        $pdf->Image($this->sahs->getAbsoluteAssetFilePath('/bundles/app/img/logo-pdf.png'), BaseTcpdf::PDF_A5_MARGIN_LEFT, BaseTcpdf::PDF_A5_MARGIN_TOP, 40);
+        $pdf->SetXY(BaseTcpdf::PDF_A5_MARGIN_LEFT, BaseTcpdf::PDF_A5_MARGIN_TOP * 2 + BaseTcpdf::MARGIN_VERTICAL_SMALL);
+
+        // invoice header
+        $pdf->setFontStyle(null, '', 11);
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.greetings').' ', '', false, 'L', false);
+        $pdf->setFontStyle(null, 'B', 11);
+        $pdf->Write(0, $receipt->getStudent()->getFullName(), '', false, 'L', true);
+        $pdf->setFontStyle(null, '', 11);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
+
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.first_paragraph'), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
+
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.second_paragraph_1'), '', false, 'L', false);
+        $pdf->setFontStyle(null, 'B', 11);
+        $pdf->Write(0, (new \DateTime())->format('d/m/Y'), '', false, 'L', false);
+        $pdf->setFontStyle(null, '', 11);
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.second_paragraph_2'), '', false, 'L', false);
+        $pdf->setFontStyle(null, 'B', 11);
+        $pdf->Write(0, $receipt->getReceiptNumber(), '', false, 'L', false);
+        $pdf->setFontStyle(null, '', 11);
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.second_paragraph_3'), '', false, 'L', false);
+        $pdf->setFontStyle(null, 'B', 11);
+        $pdf->Write(0, $receipt->getMonthNameString(), '', false, 'L', false);
+        $pdf->setFontStyle(null, '', 11);
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.second_paragraph_4'), '', false, 'L', false);
+        $pdf->setFontStyle(null, 'B', 11);
+        $pdf->Write(0, $receipt->getBaseAmountString(), '', false, 'L', false);
+        $pdf->setFontStyle(null, '', 11);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG * 2);
+
+        $pdf->Write(0, $this->ts->trans('backend.admin.receipt_reminder.third_paragraph'), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG * 0.5);
+
+        $pdf->Write(0, $this->bn, '', false, 'L', true);
+    }
+}
---
+++ src/Pdf/StudentImageRightsBuilderPdf.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Pdf;
+
+use App\Entity\Student;
+use App\Service\SmartAssetsHelperService;
+use IntlDateFormatter;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use TCPDF;
+use Exception;
+
+/**
+ * Class StudentImageRightsBuilderPdf.
+ *
+ * @category Service
+ */
+class StudentImageRightsBuilderPdf
+{
+    /**
+     * @var TCPDF
+     */
+    private $tcpdf;
+
+    /**
+     * @var SmartAssetsHelperService
+     */
+    private $sahs;
+
+    /**
+     * @var Translator
+     */
+    private $ts;
+
+    /**
+     * @var string project web title
+     */
+    private $pwt;
+
+    /**
+     * Methods.
+     */
+
+    /**
+     * StudentImageRightsBuilderPdf constructor.
+     *
+     * @param TCPDF                    $tcpdf
+     * @param SmartAssetsHelperService $sahs
+     * @param Translator               $ts
+     * @param string                   $pwt
+     */
+    public function __construct(TCPDF $tcpdf, SmartAssetsHelperService $sahs, Translator $ts, $pwt)
+    {
+        $this->tcpdf = $tcpdf;
+        $this->sahs = $sahs;
+        $this->ts = $ts;
+        $this->pwt = $pwt;
+    }
+
+    /**
+     * @param Student $student
+     *
+     * @return TCPDF
+     *
+     * @throws Exception
+     */
+    public function build(Student $student)
+    {
+        /** @var BaseTcpdf $pdf */
+        $pdf = $this->tcpdf->create($this->sahs);
+
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor($this->pwt);
+        $pdf->SetTitle($this->ts->trans('backend.admin.imagerigths.pdf_metadata_title'));
+        $pdf->SetSubject($this->ts->trans('backend.admin.imagerigths.pdf_metadata_subject'));
+        // set default font subsetting mode
+        $pdf->setFontSubsetting(true);
+        // remove default header/footer
+        $pdf->setPrintHeader(true);
+        $pdf->setPrintFooter(true);
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+        // set margins
+        $pdf->SetMargins(BaseTcpdf::PDF_MARGIN_LEFT, BaseTcpdf::PDF_MARGIN_TOP, BaseTcpdf::PDF_MARGIN_RIGHT);
+        $pdf->SetAutoPageBreak(true, BaseTcpdf::PDF_MARGIN_BOTTOM);
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+        // Add start page
+        $pdf->AddPage(PDF_PAGE_ORIENTATION, PDF_PAGE_FORMAT, true, true);
+
+        $pdf->SetXY(BaseTcpdf::PDF_MARGIN_LEFT, BaseTcpdf::PDF_MARGIN_TOP);
+        $pdf->setFontStyle(null, '', 11);
+        // Description
+        $pdf->Write(0, $this->ts->trans('backend.admin.imagerigths.description1'), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_SMALL);
+        $pdf->Write(0, $this->ts->trans('backend.admin.imagerigths.description2'), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
+        // Contact name
+        $contactName = $student->getName().' '.$student->getSurname();
+        $contactDni = $student->getDni();
+        if ($student->getParent()) {
+            $contactName = $student->getParent()->getName().' '.$student->getParent()->getSurname();
+            $contactDni = $student->getParent()->getDni();
+        }
+        $pdf->Write(0, $this->ts->trans(($student->getParent() ? 'backend.admin.imagerigths.contact_name_with_parent' : 'backend.admin.imagerigths.contact_name_without_parent'), array('%name%' => $contactName)), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_SMALL);
+        $pdf->Write(0, $this->ts->trans('backend.admin.imagerigths.contact_dni', array('%contact_dni%' => $contactDni)), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_SMALL);
+        $pdf->SetX(55);
+        $pdf->Rect(51, $pdf->GetY() + 1, 3, 3);
+        $pdf->MultiCell(125, 0, $this->ts->trans('backend.admin.imagerigths.autortization1', array('%student_name%' => $student->getName(), '%years_old%' => $student->getYearsOld())), 0, 'L', false, 1);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_SMALL);
+        $pdf->SetX(55);
+        $pdf->Rect(51, $pdf->GetY() + 1, 3, 3);
+        $pdf->MultiCell(125, 0, $this->ts->trans('backend.admin.imagerigths.autortization2', array('%student_name%' => $student->getName(), '%years_old%' => $student->getYearsOld())), 0, 'L', false, 1);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
+        // Registration date
+        $today = new \DateTime();
+        $df = new IntlDateFormatter('ca_ES', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+        $df->setPattern('MMMM');
+        $pdf->Write(0, $this->ts->trans('backend.admin.imagerigths.registration_date', array('%day%' => $today->format('j'), '%month%' => $df->format($today), '%year%' => $today->format('Y'))), '', false, 'L', true);
+        $pdf->Ln(BaseTcpdf::MARGIN_VERTICAL_BIG);
+        $pdf->setFontStyle(null, 'B', 11);
+        $pdf->Write(0, $this->ts->trans('backend.admin.imagerigths.sign'), '', false, 'L', true);
+
+        return $pdf;
+    }
+}
