7656e63b3e1f4bcf12a9a82eceb80639bb34e71d
David Romaní <david@flux.cat>
2019-11-12 16:16:17 +0100

    update some frontend FB testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,11 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import '../css/app.scss';
+import $ from 'jquery';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-require('google-charts/dist/googleCharts.esm');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
