5a3ac8cb07b828276f1cd4dccbd694dd6f035d69
David Romaní <david@flux.cat>
2019-11-12 16:42:05 +0100

    improve frontend FB calls again

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,6 @@ import 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '{{ facebook_api_key }}',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v5.0'
-    });
-    FB.AppEvents.logPageView();
-};
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 $(document).ready(function() {
     console.log('assets/js/app.js on Ready trigger');
