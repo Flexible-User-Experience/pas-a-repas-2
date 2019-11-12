442172a61157eb7d15e59ae9ba080770145f3253
David Romaní <david@flux.cat>
2019-11-12 18:47:13 +0100

    improve FB login as JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,4 +18,19 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 function statusChangeCallback(response) {
     console.log('statusChangeCallback on response trigger', response);
+    if (response.status === 'connected') {
+        console.log('connected!', response);
+    } else if (response.status === 'not_authorized') {
+        console.log('not_authorized!', response);
+    } else if (response.status === 'unknown') {
+        console.log('unknown!', response);
+    } else {
+        console.log('WARNING! NOT recognized API response status', response);
+    }
+}
+
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+        statusChangeCallback(response);
+    });
 }
