fe8f4b0cc42ad8dab20f169df938cbaaa90d092c
David Romaní <david@flux.cat>
2019-11-13 16:28:10 +0100

    improve login controllers - step 6

--- config/bundles.php
+++ config/bundles.php
@@ -34,6 +34,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     SaadTazi\GChartBundle\SaadTaziGChartBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
-    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
-    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
