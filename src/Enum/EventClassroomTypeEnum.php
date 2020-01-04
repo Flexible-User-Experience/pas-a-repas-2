6f696da8b1aca4627e4f280b5af6653634a345bb
David Romaní <david@flux.cat>
2020-01-04 13:33:48 +0100

    reduce classroom enum amount

--- src/Enum/EventClassroomTypeEnum.php
+++ src/Enum/EventClassroomTypeEnum.php
@@ -11,9 +11,6 @@ class EventClassroomTypeEnum
 {
     const CLASSROOM_1 = 0;
     const CLASSROOM_2 = 1;
-    const CLASSROOM_3 = 2;
-    const CLASSROOM_4 = 3;
-    const CLASSROOM_5 = 4;
 
     /**
      * Methods
@@ -35,9 +32,6 @@ class EventClassroomTypeEnum
         return array(
             self::CLASSROOM_1 => 'backend.admin.event.classroom_1',
             self::CLASSROOM_2 => 'backend.admin.event.classroom_2',
-            self::CLASSROOM_3 => 'backend.admin.event.classroom_3',
-            self::CLASSROOM_4 => 'backend.admin.event.classroom_4',
-            self::CLASSROOM_5 => 'backend.admin.event.classroom_5',
         );
     }
 
@@ -57,9 +51,6 @@ class EventClassroomTypeEnum
         return array(
             self::CLASSROOM_1 => 'Aula 1',
             self::CLASSROOM_2 => 'Aula 2',
-            self::CLASSROOM_3 => 'Aula 3',
-            self::CLASSROOM_4 => 'Aula 4',
-            self::CLASSROOM_5 => 'Aula 5',
         );
     }
 
@@ -79,9 +70,6 @@ class EventClassroomTypeEnum
         return array(
             self::CLASSROOM_1 => '1',
             self::CLASSROOM_2 => '2',
-            self::CLASSROOM_3 => '3',
-            self::CLASSROOM_4 => '4',
-            self::CLASSROOM_5 => '5',
         );
     }
 }
