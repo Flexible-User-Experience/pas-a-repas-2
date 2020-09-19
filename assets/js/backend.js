41d2052165a07287dd418bd3856babbe11d7044b
David Romaní <david@flux.cat>
2020-09-19 20:15:55 +0200

    copy assets from box idiomes 4 project

---
+++ assets/js/backend.js
@@ -0,0 +1,5 @@
+import '../css/backend.less';
+
+import PDFJS from 'pdfjs-dist';
+
+global.PDFJS = global.PDFJS = PDFJS;
---
+++ assets/js/frontend.js
@@ -0,0 +1,5 @@
+import '../css/frontend.less';
+import '../../node_modules/bootstrap-less/js/bootstrap.min';
+const $ = require('jquery');
+
+global.$ = global.jQuery = $;
