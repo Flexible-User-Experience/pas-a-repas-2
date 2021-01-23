98fb518a90e730dd81b75895857a48dc7ce1868d
David Romaní <david@flux.cat>
2021-01-23 19:25:18 +0100

    improve ChartsFactoryService

--- src/Service/ChartsFactoryService.php
+++ src/Service/ChartsFactoryService.php
@@ -6,52 +6,24 @@ use App\Enum\ReceiptYearMonthEnum;
 use App\Repository\InvoiceRepository;
 use App\Repository\ReceiptRepository;
 use App\Repository\SpendingRepository;
+use DateInterval;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ORM\NonUniqueResultException;
 use SaadTazi\GChartBundle\DataTable\DataRow;
 use SaadTazi\GChartBundle\DataTable\DataCell;
 use SaadTazi\GChartBundle\DataTable\DataTable;
 use SaadTazi\GChartBundle\DataTable\DataColumn;
-use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+use SaadTazi\GChartBundle\DataTable\Exception\InvalidColumnTypeException;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-/**
- * Class ChartsFactoryService.
- *
- * @category Service
- */
 class ChartsFactoryService
 {
-    /**
-     * @var Translator
-     */
-    private $ts;
+    private TranslatorInterface $ts;
+    private ReceiptRepository $rr;
+    private InvoiceRepository $ir;
+    private SpendingRepository $sr;
 
-    /**
-     * @var ReceiptRepository
-     */
-    private $rr;
-
-    /**
-     * @var InvoiceRepository
-     */
-    private $ir;
-
-    /**
-     * @var SpendingRepository
-     */
-    private $sr;
-
-    /**
-     * Methods.
-     */
-
-    /**
-     * ChartsFactoryService constructor.
-     *
-     * @param TranslatorInterface $ts
-     * @param ReceiptRepository   $rr
-     * @param InvoiceRepository   $ir
-     * @param SpendingRepository  $sr
-     */
     public function __construct(TranslatorInterface $ts, ReceiptRepository $rr, InvoiceRepository $ir, SpendingRepository $sr)
     {
         $this->ts = $ts;
@@ -63,10 +35,10 @@ class ChartsFactoryService
     /**
      * @return DataTable
      *
-     * @throws \SaadTazi\GChartBundle\DataTable\Exception\InvalidColumnTypeException
-     * @throws \Exception
+     * @throws InvalidColumnTypeException
+     * @throws NonUniqueResultException
      */
-    public function buildLastYearResultsChart()
+    public function buildLastYearResultsChart(): DataTable
     {
         $dt = new DataTable();
         $dt->addColumnObject(new DataColumn('id1', 'title', 'string'));
@@ -74,12 +46,12 @@ class ChartsFactoryService
         $dt->addColumnObject(new DataColumn('id3', $this->ts->trans('backend.admin.block.charts.expenses', array(), 'messages'), 'number'));
         $dt->addColumnObject(new DataColumn('id4', $this->ts->trans('backend.admin.block.charts.results', array(), 'messages'), 'number'));
 
-        $date = new \DateTime();
-        $date->sub(new \DateInterval('P12M'));
-        $interval = new \DateInterval('P1M');
+        $date = new DateTime();
+        $date->sub(new DateInterval('P12M'));
+        $interval = new DateInterval('P1M');
         for ($i = 0; $i <= 12; ++$i) {
             $sales = $this->rr->getMonthlyIncomingsAmountForDate($date);
-            $sales = $sales + $this->ir->getMonthlyIncomingsAmountForDate($date);
+            $sales += $this->ir->getMonthlyIncomingsAmountForDate($date);
             $expenses = $this->sr->getMonthlyExpensesAmountForDate($date);
             $results = $sales - $expenses;
             $dt->addRowObject($this->buildResultsCellsRow($date, $sales, $expenses, $results));
@@ -89,20 +61,12 @@ class ChartsFactoryService
         return $dt;
     }
 
-    /**
-     * @param \DateTime $key
-     * @param float|int $sales
-     * @param float|int $expenses
-     * @param float|int $results
-     *
-     * @return DataRow
-     */
-    private function buildResultsCellsRow($key, $sales, $expenses, $results)
+    private function buildResultsCellsRow(DateTimeInterface $key, $sales, $expenses, $results): DataRow
     {
         return new DataRow(array(
-                new DataCell(ReceiptYearMonthEnum::getShortTranslatedMonthEnumArray()[intval($key->format('n'))].'. '.$key->format('y')),
+                new DataCell(ReceiptYearMonthEnum::getShortTranslatedMonthEnumArray()[(int) $key->format('n')].'. '.$key->format('y')),
                 new DataCell($sales, number_format($sales, 0, ',', '.').'€'),
-                new DataCell($expenses, number_format($sales, 0, ',', '.').'€'),
+                new DataCell($expenses, number_format($expenses, 0, ',', '.').'€'),
                 new DataCell($results, number_format($results, 0, ',', '.').'€'),
             )
         );
