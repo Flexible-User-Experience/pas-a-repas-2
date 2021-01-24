1f4fbf5bb05665f78392b8dc81974c82dad0c6bd
David Romaní <david@flux.cat>
2021-01-24 13:13:27 +0100

    improve frontend homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactMessage;
 use App\Entity\Invoice;
 use App\Entity\NewsletterContact;
-use App\Entity\Teacher;
-use App\Form\Type\ContactHomepageType;
-use App\Manager\MailchimpManager;
+use App\Form\Type\ContactMessageType;
+use App\Service\GoogleMapsService;
 use App\Service\NotificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,46 +22,79 @@ class DefaultController extends AbstractController
 {
     const ENV_DEV = 'dev';
 
+
     /**
      * @Route("/", name="app_homepage")
      */
-    public function indexAction(Request $request, MailchimpManager $mailchimpManager, NotificationService $messenger): Response
+    public function indexAction(Request $request, GoogleMapsService $gms, NotificationService $messenger): Response
     {
-        $teachers = $this->getDoctrine()->getRepository(Teacher::class)->findAllEnabledSortedByPosition();
-
-        $contact = new NewsletterContact();
-        $newsletterForm = $this->createForm(ContactHomepageType::class, $contact);
-        $newsletterForm->handleRequest($request);
-
-        if ($newsletterForm->isSubmitted() && $newsletterForm->isValid()) {
-            // Persist new contact message into DB
+        $flash = null;
+        $mapObject = $gms->buildMap(40.7061278, 0.5817055555555556, 15);
+        $contactEntity = new ContactMessage();
+        $form = $this->createForm(ContactMessageType::class, $contactEntity);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // persist entity
             $em = $this->getDoctrine()->getManager();
-            $em->persist($contact);
+            $contactEntity->setDescription('');
+            $em->persist($contactEntity);
             $em->flush();
-            // Subscribe contact to mailchimp list
-            $result = $mailchimpManager->subscribeContactToList($contact, $this->getParameter('mailchimp_list_id'));
-
-            if (is_array($result) && MailchimpManager::SUBSCRIBED === $result['status']) {
-                // Send notification and OK flash
-                $this->setFlashMessageAndEmailNotifications($messenger, $contact);
-                // Clean up new form
-                $contact = new NewsletterContact();
-                $newsletterForm = $this->createForm(ContactHomepageType::class, $contact);
-            } else {
-                // Mailchimp error
-                $this->addFlash(
-                    'danger',
-                    'S\'ha produït un error durant el procés de registre al newsletter. Torna a provar-ho més tard o contacta a través del formulari web.'
-                );
-            }
+            // send notifications
+            $messenger->sendUserNotification($contactEntity);
+            $messenger->sendAdminNotification($contactEntity);
+            // reset form
+            $contactEntity = new ContactMessage();
+            $form = $this->createForm(ContactMessageType::class, $contactEntity);
+            // build flash message
+            $flash = 'frontend.index.main.sent';
         }
 
         return $this->render('Front/homepage.html.twig',
             [
-                'teachers' => $teachers,
-                'newsletterForm' => $newsletterForm->createView(),
+                'mapView' => $mapObject,
+                'contactForm' => $form->createView(),
+                'flash' => $flash,
             ]
         );
+
+//
+//
+//
+//        $teachers = $this->getDoctrine()->getRepository(Teacher::class)->findAllEnabledSortedByPosition();
+//
+//        $contact = new NewsletterContact();
+//        $newsletterForm = $this->createForm(ContactHomepageType::class, $contact);
+//        $newsletterForm->handleRequest($request);
+//
+//        if ($newsletterForm->isSubmitted() && $newsletterForm->isValid()) {
+//            // Persist new contact message into DB
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($contact);
+//            $em->flush();
+//            // Subscribe contact to mailchimp list
+//            $result = $mailchimpManager->subscribeContactToList($contact, $this->getParameter('mailchimp_list_id'));
+//
+//            if (is_array($result) && MailchimpManager::SUBSCRIBED === $result['status']) {
+//                // Send notification and OK flash
+//                $this->setFlashMessageAndEmailNotifications($messenger, $contact);
+//                // Clean up new form
+//                $contact = new NewsletterContact();
+//                $newsletterForm = $this->createForm(ContactHomepageType::class, $contact);
+//            } else {
+//                // Mailchimp error
+//                $this->addFlash(
+//                    'danger',
+//                    'S\'ha produït un error durant el procés de registre al newsletter. Torna a provar-ho més tard o contacta a través del formulari web.'
+//                );
+//            }
+//        }
+//
+//        return $this->render('Front/homepage.html.twig',
+//            [
+//                'teachers' => $teachers,
+//                'newsletterForm' => $newsletterForm->createView(),
+//            ]
+//        );
     }
 
     /**
