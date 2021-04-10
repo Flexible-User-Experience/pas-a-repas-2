1b830c5d82fb2dd96324ac79c30003e0d3f7905c
David Romaní <david@flux.cat>
2021-04-10 20:59:09 +0200

    remove unnecessary pdfjs Yarn dependency

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,5 +1,1 @@
 import '../css/backend.less';
-
-import PDFJS from 'pdfjs-dist';
-
-global.PDFJS = global.PDFJS = PDFJS;
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,7 @@ Encore
         {from: './node_modules/ckeditor4/lang', to: 'ckeditor4/lang/[path][name].[ext]'},
         {from: './node_modules/ckeditor4/plugins', to: 'ckeditor4/plugins/[path][name].[ext]'},
         {from: './node_modules/ckeditor4/skins', to: 'ckeditor4/skins/[path][name].[ext]'},
-        {from: './node_modules/google-charts/dist', to: 'google-charts/[path][name].[ext]'},
-        {from: './node_modules/pdfjs-dist/build', to: 'pdfjs/[path][name].[ext]'}
+        {from: './node_modules/google-charts/dist', to: 'google-charts/[path][name].[ext]'}
     ])
     // public path used by the web server to access the output path
     .setPublicPath('/build')
