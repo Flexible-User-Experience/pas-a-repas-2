cc49a36e33da3834964a2c8310e1b787f2728a78
David Romaní <david@flux.cat>
2021-09-10 10:07:55 +0200

    fix Blog controller problems

--- src/Entity/Traits/PositionTrait.php
+++ src/Entity/Traits/PositionTrait.php
@@ -4,40 +4,21 @@ namespace App\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * Position trait
- *
- * @category Trait
- */
 Trait PositionTrait
 {
     /**
-     * @var integer
-     *
      * @ORM\Column(type="integer")
      */
-    private $position;
+    private int $position;
 
-    /**
-     * Set position
-     *
-     * @param string $position
-     *
-     * @return $this
-     */
-    public function setPosition($position)
+    public function setPosition(int $position): self
     {
         $this->position = $position;
 
         return $this;
     }
 
-    /**
-     * Get position
-     *
-     * @return string
-     */
-    public function getPosition()
+    public function getPosition(): int
     {
         return $this->position;
     }
