e5d501b6d62d3be0d1f16364ce18c589be2471f7
David Romaní <david@flux.cat>
2021-09-15 11:46:22 +0200

    add symfony/ux-chartjs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,12 @@
 Changelog
 =========
 
-##### Version 4.5.11 (WIP)
+##### Version 4.5.12 (WIP)
  * fix minor bugs
+
+##### Version 4.5.11 (2021-09-15)
+ * replace 'callto' by 'tel' phone tags
+ * add Whatsapp contact message button
 
 ##### Version 4.5.10 (2021-09-13)
  * remove duplicated frontend layout template
--- config/bundles.php
+++ config/bundles.php
@@ -37,8 +37,9 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
