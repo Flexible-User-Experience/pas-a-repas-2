8e10486819b8f2f23b8d7a5fb87c7fd3ded2aabd
David Romaní <david@flux.cat>
2022-08-04 13:04:08 +0200

    add knplabs/knp-paginator-bundle missing dependency

--- config/bundles.php
+++ config/bundles.php
@@ -36,4 +36,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
